9348bee56051bae7461e03c088a9803614efeeb2
Kevin <knphillips01@gmail.com>
2019-09-10 15:55:56 -0500

    added routing to app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/home';
 import Recipes from './pages/recipes';
 import SingleRecipe from './pages/singlerecipe';
 import Error from './pages/error';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home /> 
-        <Recipes />
-        <SingleRecipe />
-        <Error />
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/recipes' component={Recipes} />
+            <Route path='/recipes/:id' component={SingleRecipe} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
