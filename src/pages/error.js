3b368a2a8f2bc3f6d0a0fe39949f8670530f257b
Kevin <knphillips01@gmail.com>
2019-09-10 17:15:44 -0500

    added default/error page

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import Header from '../components/header';
+import {Link} from 'react-router-dom';
 
 class Error extends Component {
   render() {
-    return <div>
-      testing from error
-    </div>
+    return (
+    <Header title='404' styleClass='default-hero'>
+      <h2 className='text-light text-uppercase'>You're in the wrong place</h2>
+      <Link 
+        to='/'
+        className='text-uppercase btn btn-secondary btn-lg mt-3'
+        >
+        return home
+      </Link>
+    </Header>
+    )
   }
 }
 
