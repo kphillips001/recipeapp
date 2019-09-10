8205e08e5a9dacc536805f697dd6fb1745187dc4
Kevin <knphillips01@gmail.com>
2019-09-10 16:54:56 -0500

    added header component, CTA, and background image to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import Header from '../components/header';
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
   render() {
-    return <div>
-      testing from home
-    </div>
+    return (
+    <Header title='fabulous recipes'>
+      <Link 
+        to='recipes' 
+        className='text-uppercase btn btn-secondary btn-lg mt-3'>
+        search recipes
+        </Link>
+    </Header>
+    );
   }
 }
 
