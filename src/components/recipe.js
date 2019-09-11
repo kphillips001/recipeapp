95f9303fdb0221c7ea27028735feb600fb51579a
Kevin <knphillips01@gmail.com>
2019-09-10 21:12:21 -0500

    built out recipe component

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Recipe extends Component {
   render() {
-    return <div>hello from recipe</div>;
+    const{image_url, title, source_url, publisher, recipe_id} = this.props.recipe;
+
+    return <div className='col-10 mx-auto col-md-6 col-lg-4 my-3'>
+      <div className='card' style={{ height: '100%'}} >
+        <img 
+          src={image_url} 
+          style={{ height: '14rem'}} 
+          className='img-card-top' 
+          alt='recipe'
+          />
+        <div className='card-body text-capitalize'>
+          <h6>{title}</h6>
+          <h6 className='text-warning text-slanted'>provided by {publisher}</h6>
+        </div>
+        <div className='card-footer'>
+          <Link to={`/recipes/${recipe_id}`} className='btn btn-info text-capitalize'>
+            details
+          </Link>
+          <a href={source_url} target='_blank' rel='noopener noreferrer'     className='btn btn-warning mx-2 text-capitalize'>
+            recipe Url
+          </a>
+        </div>
+      </div>
+    </div>
   }
 }
 
