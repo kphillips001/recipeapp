63c9a798a41c4aea24aa6e9734b3c5a2b7e099f8
Kevin <knphillips01@gmail.com>
2019-09-11 12:36:18 -0500

    added font awesome for search button on recipe page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Home from './pages/home';
 import Recipes from './pages/recipes';
 import SingleRecipe from './pages/singlerecipe';
